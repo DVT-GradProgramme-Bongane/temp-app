ba5612c2fd62dcea545261c524efde65ce0743d5
Bongane Lebopo <blebopo@dvtsoftware.com>
2026-02-27 13:35:20 +0200

    Feature: Temperature state shared between celcius and fahrenheit

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,16 +1,37 @@
+import { useState, type BaseSyntheticEvent } from "react";
+
 export default function HomePage() {
+    const [temperature, setTemperature] = useState<number>();
     
+    function handleTempOnChange(event: BaseSyntheticEvent){
+      setTemperature(event.target.value);
+    }
 
     return (
     <>
+      <header>
+        <h1 className="temperature-header">
+            Temperature Converter
+        </h1>
+      </header>
+
+      
       <div className="temperature-input-output-container">
         <div className="temperature-input">
           <label htmlFor="celsius">Celcius</label>
-          <input id="celsius"/>
+          <input 
+             id="celsius" 
+             value={temperature}
+             onChange={handleTempOnChange}
+          />
         </div>
         <div className="temperature-input">
-          <label htmlFor="fahrenheit">Fahrenheight</label>
-          <input id="fahrenheit"/>
+          <label htmlFor="fahrenheit">Fahrenheit</label>
+          <input 
+            id="fahrenheit" 
+            value={temperature}
+            onChange={handleTempOnChange}
+            />
         </div>
       </div>
     </>
