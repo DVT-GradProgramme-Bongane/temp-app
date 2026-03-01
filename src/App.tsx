7f810d964e4705eb5048eb7459c205b6a8c13d02
Bongane Lebopo <blebopo@dvtsoftware.com>
2026-03-01 22:32:51 +0200

    Feature: Page routing added.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
+import { BrowserRouter, Route, Routes } from "react-router";
 import "./App.css";
 import HomePage from "./pages/Home";
+import PageNotFoundPage from "./pages/PageNotFound";
 
 function App() {
   return (
     <>
-      <HomePage></HomePage>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage/>}/>
+          <Route path="*" element={<PageNotFoundPage/>}/>
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
---
+++ src/pages/PageNotFound.tsx
@@ -0,0 +1,5 @@
+export default function PageNotFoundPage() {
+    return(
+        <p>Page not found...</p>
+    )
+}
