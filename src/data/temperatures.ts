aa5c82f084d489c7e5df8bf448b814217df7a389
Bongane Lebopo <blebopo@dvtsoftware.com>
2026-03-01 19:20:55 +0200

    Semantics: Prettified code.

--- src/data/temperatures.ts
+++ src/data/temperatures.ts
@@ -1,7 +1,7 @@
 interface Temperature {
   type: string;
   calculate: boolean;
-  value: number ;
+  value: number;
   convertToKelvin: (value: number) => number;
   convertFromKelvin: (kelvin: number) => number;
 }
@@ -12,23 +12,23 @@ export const temperatureData: Temperature[] = [
     calculate: false,
     value: 0,
     convertToKelvin: (celcius: number) => {
-      return (celcius + 273.15);
+      return celcius + 273.15;
     },
-    convertFromKelvin : (kelvin: number) => {
+    convertFromKelvin: (kelvin: number) => {
       let celcius = kelvin - 273.15;
       return celcius;
-    }
+    },
   },
   {
     type: "fahrenheit",
     calculate: false,
-    value : 0,
+    value: 0,
     convertToKelvin: (fahrenheit: number) => {
       return (fahrenheit - 32) * (5 / 9) + 273.15;
     },
     convertFromKelvin: (kelvin: number) => {
-        let fahrenheit = (kelvin - 273.15) * (9/5) + 32;
-        return fahrenheit;
-    }
+      let fahrenheit = (kelvin - 273.15) * (9 / 5) + 32;
+      return fahrenheit;
+    },
   },
 ];
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,33 +3,41 @@ import { temperatureData } from "../data/temperatures";
 
 export default function HomePage() {
   const [temperatures, setTemperature] = useState(temperatureData);
-  
+
   function handleTempOnChange(event: BaseSyntheticEvent) {
-    const kelvinUpdate = temperatures.find( temperature => 
-      temperature.type === event.target.id);
+    const kelvinUpdate = temperatures.find(
+      (temperature) => temperature.type === event.target.id,
+    );
     console.log(kelvinUpdate);
     let newTemperatureStates = temperatures
       .map(
-        temp => {
-            if(temp.type === event.target.id){
-              temp.calculate = false;  
-            }
-            else {
-              temp.calculate = true;
-            }
-            console.log(temp);
-            return {...temp, value : kelvinUpdate?.convertToKelvin(Number(event.target.value))};
-        })
-      .map( 
-        temp => {
-          if(temp.calculate){
-            return {...temp, value: temp.convertFromKelvin(temp.value ? temp.value: 0)}
+        // Intermediate step to create a "normalized" value or value that
+        // all temperature units can convert from including temperature scales
+        // such as the Rankine scale etc.
+        (temp) => {
+          // A 2 step mapping, determines which value should not be calculated
+          // and which shoud as well as "normalizing" the scale
+          if (temp.type === event.target.id) {
+            temp.calculate = false;
+          } else {
+            temp.calculate = true;
           }
-          else {
-            return {...temp, value: Number(event.target.value)};
-          }
+          return {
+            ...temp,
+            value: kelvinUpdate?.convertToKelvin(Number(event.target.value)),
+          };
+        },
+      )
+      .map((temp) => {
+        if (temp.calculate) {
+          return {
+            ...temp,
+            value: temp.convertFromKelvin(temp.value ? temp.value : 0),
+          };
+        } else {
+          return { ...temp, value: Number(event.target.value) };
         }
-       )
+      });
     console.log(newTemperatureStates);
     setTemperature(newTemperatureStates);
   }
@@ -41,18 +49,18 @@ export default function HomePage() {
       </header>
 
       <div className="temperature-input-output-container">
-        {temperatures.map( temperature => {
-            return (
-                <div className="temperature-input">
-                  <label htmlFor={temperature.type}>{temperature.type}</label>
-                  <input
-                    type="number"
-                    id={temperature.type}
-                    value={temperature.value}
-                    onChange={handleTempOnChange}
-                  />
-                </div>
-            );
+        {temperatures.map((temperature) => {
+          return (
+            <div className="temperature-input">
+              <label htmlFor={temperature.type}>{temperature.type}</label>
+              <input
+                type="number"
+                id={temperature.type}
+                value={temperature.value}
+                onChange={handleTempOnChange}
+              />
+            </div>
+          );
         })}
         {/* <div className="temperature-input">
           <label htmlFor="celsius">Celcius</label>
